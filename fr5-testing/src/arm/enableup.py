7de650eab5e510d6e81f31cdd9ee1ad95ac5febc
ethan <enkhtushig1214@gmail.com>
2026-04-29 17:10:49 -0400

    refactor: fr5-testing into root

--- fr5-testing/src/arm/enableup.py
+++ fr5-testing/src/arm/enableup.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+import sys
+
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+
 from fairino import Robot
 import time
 
@@ -6,10 +12,11 @@ import time
 robot = Robot.RPC("192.168.58.2")
 
 
-robot.RobotEnable(0)
-#time.sleep(5)
-input("Press enter when ready to enable:")
 robot.RobotEnable(1)
+#time.sleep(5)
+#input("Press enter when ready to enable:")
+#robot.RobotEnable(1)
 
+robot.MoveJ(joint_pos = [-90,-90,-90,-90,90,-90], user = 0, tool= 0)
 
-robot.CloseRPC()
+#robot.CloseRPC()
